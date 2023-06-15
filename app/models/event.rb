533ee563828622c6ef52de2e17bccb614c318015
Jose Aguilar <jaguilarny@gmail.com>
2023-06-15 00:22:40 -0400

    add validation for max_attendee limit

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,5 +4,5 @@ class Event < ApplicationRecord
   belongs_to :organizer, class_name: "User", foreign_key: "organizer_id"
 
   validates :event_name, :event_city, :event_start, :event_end, :max_attendees, :event_description, presence: true
-  validates :max_attendees, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+  validates :max_attendees, numericality: { only_integer: true, less_than_or_equal_to: 100, greater_than: 2 }
 end
