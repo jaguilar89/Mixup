7c25b0e9fe8dd78659871049ef861d912050b530
jaguilar89 <jaguilarny@gmail.com>
2024-01-17 23:00:40 -0500

    minor fix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,7 +8,7 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 # builds the back end code
-#bundle install
+bundle install
 #RAILS_ENV=production rake db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-#bundle exec rake db:migrate 
+bundle exec rake db:migrate 
 #bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
